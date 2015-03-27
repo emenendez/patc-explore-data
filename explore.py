e3703c03a2eb19beeb30745f70a0778a1e3047a1
Eric Menendez <ericmenendez@gmail.com>
2015-03-26 23:10:43 -0400

    exception handling and prefixes

--- explore.py
+++ explore.py
@@ -3,12 +3,14 @@
 import argparse
 from pathlib import Path
 import xml.etree.ElementTree as ET
+import sys
 
 
 def prefix(tags):
     prefixes = [
+        '{http://www.topografix.com/GPX/1/0}',
         '{http://www.topografix.com/GPX/1/1}',
-        '{http://www.topografix.com/GPX/1/1}',
+        '{http://www.trimbleoutdoors.com/WebServices/Api/1/0}',
         ]
 
     for tag in tags:
@@ -29,22 +31,26 @@ def main():
 
     for pattern in args.file:
         for infile in Path('.').glob(pattern):
-            root = ET.parse(str(infile)).getroot()
-            for child in root:
-                if args.top:
-                    if child.tag not in prefix(['trk', 'wpt']):
-                        ET.dump(child)
-                if args.track:
-                    if child.tag in prefix(['trk']):
-                        for subchild in child:
-                            if subchild.tag not in prefix(['name', 'trkseg']):
-                                ET.dump(subchild)
-                if args.waypoint:
-                    if child.tag in prefix(['wpt']):
-                        for subchild in child:
-                            if subchild.tag not in prefix(['ele', 'time', 'name', 'desc', 'cmt', 'lat', 'lon', 'ltime']):
-                                ET.dump(subchild)
+            try:
+                root = ET.parse(str(infile)).getroot()
+                
+                for child in root:
+                    if args.top:
+                        if child.tag not in prefix(['trk', 'wpt']):
+                            ET.dump(child)
+                    if args.track:
+                        if child.tag in prefix(['trk']):
+                            for subchild in child:
+                                if subchild.tag not in prefix(['name', 'trkseg']):
+                                    ET.dump(subchild)
+                    if args.waypoint:
+                        if child.tag in prefix(['wpt']):
+                            for subchild in child:
+                                if subchild.tag not in prefix(['ele', 'time', 'name', 'desc', 'cmt', 'lat', 'lon', 'ltime']):
+                                    ET.dump(subchild)
 
+            except Exception:
+                print('Error: could not parse {}'.format(infile), file=sys.stderr)
 
 if __name__ == "__main__":
     main()
